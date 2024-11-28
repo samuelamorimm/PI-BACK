7f5ae61ca60bc66d929a631d60dc5d60b2c7446a
samuelamorimm <samuelhenriqueamorimm@gmail.com>
2024-11-28 14:01:30 -0300

    update

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='landing-page'),
     path('blog/', views.post_list, name='post_list'),
     path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
     path('blog/post/new/', views.post_create, name='post_create'),
